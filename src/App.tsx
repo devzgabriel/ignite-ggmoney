e8b271ddfa9399027199a581856518c68bb33acc
Gabriel Silva <devzgabriel@gmail.com>
2021-03-20 17:48:42 -0300

    modal styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import { Dashboard } from './components/Dashboard'
+import { useState } from 'react'
 import { Header } from './components/Header'
+import { Dashboard } from './components/Dashboard'
+import { NewTransactionModal } from './components/NewTransactionModal'
 import { GlobalStyle } from './styles/global'
 
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
+
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  )
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   )
